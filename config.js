7b64e36410c2f25fbbb0cbb76223ab80e87b7efd
Heet Dudhwala <heetdudhwala848@gmail.com>
2025-05-30 12:09:40 +0530

    initial commit

---
+++ config.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:5000/bitespeed', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("MongoDB Connected");
+    } catch (error) {
+        console.log("Error occurred", error);
+    }
+};
